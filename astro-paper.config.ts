3daf70df8dcb1a262cf716ab71e84e36cff2803a
huukhai <huukhai@sepay.vn>
2026-05-23 03:48:44 +0000

    feat: fix title and add social share

---
+++ astro-paper.config.ts
@@ -0,0 +1,6 @@
+export const shareLinks = [
+  { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+  { name: "x", url: "https://x.com/intent/post?url=" },
+  { name: "telegram", url: "https://t.me/share/url?url=" },
+  { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+];
