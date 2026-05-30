a25022969a4832f449ff537c0aaacabb0bb9318d
huukhai <huukhai@sepay.vn>
2026-05-30 01:40:53 +0000

    save

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'MangaK';
 export const SITE_DESCRIPTION = 'Trang review truyện tranh Manga, Manhua, Manhwa uy tín hàng đầu Việt Nam';
+export const DEFAULT_OG_IMAGE = 'https://cdn.khaizinam.io.vn/blog-folder/2026-05/og-banner.jpg';
+
